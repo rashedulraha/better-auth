ba3265931c82270dd027afa776f170c941d4f924
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-21 14:59:38 +0600

    adding route and and setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,15 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* header */}
+        <header  className="border-b border-gray-700">
+          <nav className="flex items-center justify-center gap-4 container mx-auto px-4 py-4 ">
+            <Link href={"/"}>Home</Link>
+            <Link href={"/dashboard"}>Dashboard</Link>
+            <Link href={"/signin"}>Sign in</Link>
+            <Link href={"/signup"}>Sign up</Link>
+          </nav>
+        </header>
         {children}
       </body>
     </html>
---
+++ src/app/signin/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SignIN = () => {
+  return (
+    <div>
+      <h1>Sign in Page</h1>
+    </div>
+  );
+};
+
+export default SignIN;
